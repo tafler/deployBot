7d950e5767f6b3b24952ed709ffabf82f9245a62
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2019-09-17 21:47:53 +0300

    Some refactoring (#2)
    * installed needed packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    "ecmaVersion": 2019,
-    "sourceType": "module"
+    ecmaVersion: 2019,
+    sourceType: 'module',
   },
   plugins: ['prettier'],
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   rules: {
     'prettier/prettier': 'error',
+    'no-console': 'off',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log('init');
+import 'dotenv/config';
+import express from 'express';
+import { Application } from 'express';
+import { getMe, postMe } from './telegramController';
+
+const app: Application = express();
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+app.get('/', getMe);
+
+app.post('/', postMe);
+
+app.listen(port, () => {
+  console.warn(`Server started on ${hostname}:${port}`);
+});
